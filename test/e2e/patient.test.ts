eed26e35aebf45ed14ac462a24a3ba33f9f29f15
Razieh Akbari <rakbari@hypertvx.com>
2023-02-04 19:53:11 +0100

    Patient multiple name e2e

--- test/e2e/patient.test.ts
+++ test/e2e/patient.test.ts
@@ -1,8 +1,10 @@
 import * as fs from 'fs';
 import path from 'path';
 
-import patientSingleIdentifier from './fixtures/result/patient.single.identifier.json';
-import patientMultipleIdentifier from './fixtures/result/patient.multiple.identifier.json';
+import patientSingleIdentifier from './fixtures/result/0-patient.single.identifier.json';
+import patientMultipleIdentifier from './fixtures/result/1-patient.multiple.identifier.json';
+import patientMultipleName from './fixtures/result/2-patient.multiple.name.json';
+
 import Compile from '../../src/main';
 import { Patient } from 'fhir/r4';
 describe('Patient e2e', () => {
@@ -11,7 +13,7 @@ describe('Patient e2e', () => {
       const file = fs.readFileSync(
         path.resolve(
           __dirname,
-          './fixtures/input/patient.single.identifier.txt'
+          './fixtures/input/0-patient.single.identifier.txt'
         )
       );
       let patient = Compile(file);
@@ -22,12 +24,20 @@ describe('Patient e2e', () => {
       const file = fs.readFileSync(
         path.resolve(
           __dirname,
-          './fixtures/input/patient.multiple.identifier.txt'
+          './fixtures/input/1-patient.multiple.identifier.txt'
         )
       );
       let patient = Compile(file);
 
       expect(patient).toEqual(patientMultipleIdentifier);
+    });
+    it('should set multiple name', () => {
+      const file = fs.readFileSync(
+        path.resolve(__dirname, './fixtures/input/2-patient.multiple.name.txt')
+      );
+      let patient = Compile(file);
+
+      expect(patient).toEqual(patientMultipleName);
     });
   });
 });
