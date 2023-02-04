405e14f887706cdf0411a0ad44018be1ced4e80b
Razieh Akbari <rakbari@hypertvx.com>
2023-02-04 19:35:13 +0100

    manage multiple array value

--- test/e2e/patient.test.ts
+++ test/e2e/patient.test.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import path from 'path';
 
 import patientSingleIdentifier from './fixtures/result/patient.single.identifier.json';
+import patientMultipleIdentifier from './fixtures/result/patient.multiple.identifier.json';
 import Compile from '../../src/main';
 import { Patient } from 'fhir/r4';
 describe('Patient e2e', () => {
@@ -16,6 +17,17 @@ describe('Patient e2e', () => {
       let patient = Compile(file);
 
       expect(patient).toEqual(patientSingleIdentifier);
+    });
+    it('should set multiple identifer', () => {
+      const file = fs.readFileSync(
+        path.resolve(
+          __dirname,
+          './fixtures/input/patient.multiple.identifier.txt'
+        )
+      );
+      let patient = Compile(file);
+
+      expect(patient).toEqual(patientMultipleIdentifier);
     });
   });
 });
