fbaf42eac11d14b108d383a5e898ae8f694c2b5e
Razieh Akbari <rakbari@hypertvx.com>
2023-02-21 22:55:13 +0100

    Add chart js

--- comfhirer-ri/src/components/dashboard/Dashboard.tsx
+++ comfhirer-ri/src/components/dashboard/Dashboard.tsx
@@ -1,7 +1,52 @@
 import { Grid } from '@mui/material';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Title,
+  PointElement,
+} from 'chart.js';
+import { Bar, Bubble, Doughnut } from 'react-chartjs-2';
+import { data as doughnutData } from './fixtures/doughnut';
+import { data as barData, options as barOptions } from './fixtures/bar';
+import {
+  data as bubbleData,
+  options as bubbleOptions,
+} from './fixtures/bubble';
 
+ChartJS.register(
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend
+);
 function Dashoard() {
-  return <Grid data-testid='dashboard'></Grid>;
+  return (
+    <Grid
+      container
+      data-testid='dashboard'
+      height='60vh'
+      justifyContent='space-evenly'
+    >
+      <Grid item sm={3} alignSelf='center'>
+        <Doughnut data={doughnutData} />
+      </Grid>
+      <Grid item sm={3} alignSelf='center'>
+        <Bar options={barOptions} data={barData} />;
+      </Grid>
+      <Grid item sm={3} alignSelf='center'>
+        <Bubble options={bubbleOptions} data={bubbleData} />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Dashoard;
--- comfhirer-ri/src/components/layout/components/FixedDrawer.tsx
+++ comfhirer-ri/src/components/layout/components/FixedDrawer.tsx
@@ -1,31 +1,56 @@
-import {
-  CoffeeMaker,
-  Dashboard,
-  LocalFireDepartmentSharp,
-} from '@mui/icons-material';
+import { Dashboard, LocalFireDepartmentSharp } from '@mui/icons-material';
 import { Box, styled } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function FixedDrawer() {
   const FixedBox = styled(Box)({
     position: 'fixed',
     left: 0,
     top: 64,
-    height: '200px',
+    height: '100%',
     width: '60px',
     display: 'flex',
     flexFlow: 'column',
     paddingTop: '2rem',
-    justifyContent: 'space-between',
+    justifyContent: 'start',
   });
+
+  const LinkStyler = styled('div')(({ theme }) => ({
+    width: '100%',
+    textAlign: 'center',
+    margin: '1rem',
+    border: '1px solid green',
+    borderRight: '0px',
+    borderRadius: '30% 0',
+    marginRight: '-1px',
+    background: 'white',
+    padding: '3px',
+    '&:hover': {
+      backgroundColor: theme.palette.primary.light,
+    },
+  }));
   return (
-    <FixedBox>
-      <Link to='/' style={{ alignSelf: 'center' }} aria-label='dashboard'>
-        <Dashboard color='secondary' fontSize='large' />
-      </Link>
-      <Link to='caller' style={{ alignSelf: 'center' }} aria-label='caller'>
-        <LocalFireDepartmentSharp color='secondary' fontSize='large' />
-      </Link>
+    <FixedBox sx={{ borderRight: '1px solid #33ab9f70' }}>
+      <LinkStyler>
+        <NavLink
+          className={({ isActive }) => (isActive ? '.Mui-active' : undefined)}
+          to='/'
+          style={{ alignSelf: 'center' }}
+          aria-label='dashboard'
+        >
+          <Dashboard color='secondary' fontSize='large' />
+        </NavLink>
+      </LinkStyler>
+      <LinkStyler>
+        <NavLink
+          className={({ isActive }) => (isActive ? '.Mui-active' : undefined)}
+          to='caller'
+          style={{ alignSelf: 'center' }}
+          aria-label='caller'
+        >
+          <LocalFireDepartmentSharp color='secondary' fontSize='large' />
+        </NavLink>
+      </LinkStyler>
     </FixedBox>
   );
 }
